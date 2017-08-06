5d520bedf519327cccdc6bb64cd5180b3efeec20
TJHL <league@WTS9.attlocal.net>
2017-08-06 14:26:18 -0700

    initial commit

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,6 +1,69 @@
 package threads;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class OlympicRings_Threaded {
+	public static void main(String[] args) {
+		Robot a = new Robot(400, 700);
+		Robot b = new Robot(600, 500);
+		Robot c = new Robot(800, 700);
+		Robot d = new Robot(1000, 500);
+		Robot e = new Robot(1200, 700);
+
+		
+		new Thread(()->{
+			a.penDown();
+			for (int i = 0; i < 360; i++) {
+				a.setSpeed(10);
+				a.setPenColor(Color.blue);
+				a.move(3);
+				a.turn(1);
+			}
+		}).start(); 
+		
+		new Thread(()->{
+			b.penDown();
+			for (int i = 0; i < 360; i++) {
+				b.setSpeed(10);
+				b.setPenColor(Color.yellow);
+				b.move(3);
+				b.turn(1);
+			}
+		}).start(); 
+		
+		new Thread(()->{
+			c.penDown();
+			for (int i = 0; i < 360; i++) {
+				c.setSpeed(10);
+				c.setPenColor(Color.black);
+				c.move(3);
+				c.turn(1);
+			}
+		}).start(); 
+		
+		new Thread(()->{
+			d.penDown();
+			for (int i = 0; i < 360; i++) {
+				d.setSpeed(10);
+				d.setPenColor(Color.green);
+				d.move(3);
+				d.turn(1);
+			}
+		}).start(); 
+		
+		new Thread(()->{
+			e.penDown();
+			for (int i = 0; i < 360; i++) {
+				e.setSpeed(10);
+				e.setPenColor(Color.red);
+				e.move(3);
+				e.turn(1);
+			}
+		}).start(); 
+	}
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
 
 }
--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,5 +1,23 @@
+package threads;
 
 public class ThreadedReverseGreeting {
+	int counter = 1;
+
+	public static void main(String[] args) {
+		int number = 50;
+		ThreadedReverseGreeting runner = new ThreadedReverseGreeting();
+		runner.createThread(number);
+	}
+	public void createThread(int num){
+		if(counter<51){
+			new Thread(()->{
+				createThread(num-1);
+				System.out.println("Hello from Thread " + num  + "!");
+				counter++;
+				
+			}).start();
+		}
+	}
   //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
   //Thread 2 creates Thread 3; and so on, up to Thread 50. 
   //Each thread should print "Hello from Thread <num>!", 
